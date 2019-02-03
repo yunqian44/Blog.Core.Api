2a6b9ca1b6a0ed1fa14f222f76fe610a4eb2fa5a
刺客 <1350126839@qq.com>
2019-02-03 22:58:48 +0800

    新增Core类库方法

---
+++ Demo.Core.Api.Core/Extension/EnumExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Demo.Core.Api.Core.Extension
+{
+    public static class EnumExtension
+    {
+        public static string GetDescription(this Enum o)
+        {
+            Type enumType = o.GetType();
+            string name = Enum.GetName(enumType, o);
+            var customAttribute = (DescriptionAttribute)Attribute.GetCustomAttribute(enumType.GetField(name), typeof(DescriptionAttribute));
+            if (customAttribute != null)
+            {
+                return customAttribute.Description;
+            }
+            return name;
+        }
+    }
+}

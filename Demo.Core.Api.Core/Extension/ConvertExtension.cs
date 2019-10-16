cddb34ba5d941ffa2d02715038ec58879d4aad46
贠乾 <yunqian8@live.com>
2019-10-16 18:27:51 +0800

    新增Appsetting 操作方法 新增Int类型的扩展方法

---
+++ Demo.Core.Api.Core/Extension/ConvertExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Core.Api.Core.Extension
+{
+    public static class ConvertExtension
+    {
+        public static int ToInt(this object obj)
+        {
+            int reval = 0;
+            if (obj == null) return 0;
+            if (obj != null && obj != DBNull.Value && int.TryParse(obj.ToString(), out reval))
+            {
+                return reval;
+            }
+            return reval;
+        }
+
+        public static string ObjToString(this object thisValue)
+        {
+            if (thisValue != null) return thisValue.ToString().Trim();
+            return string.Empty;
+        }
+    }
+}

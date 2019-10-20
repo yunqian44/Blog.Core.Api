5516bfced1583df7d563a34d78cdc10354303166
贠乾 <yunqian8@live.com>
2019-10-20 23:07:54 +0800

    新增泛型仓储接口及其实现

--- Demo.Core.Api.IRepository/IAdvertisementRepository.cs
+++ Demo.Core.Api.IRepository/IAdvertisementRepository.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Demo.Core.Api.Model.Entity;
+using System;
 
 namespace Demo.Core.Api.IRepository
 {
-    public interface IAdvertisementRepository
+    public interface IAdvertisementRepository:
+        IBaseRepository<Advertisement>
     {
         int Sum(int i, int j);
     }

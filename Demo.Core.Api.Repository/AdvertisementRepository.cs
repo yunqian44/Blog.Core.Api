3b8d788728fd92c576cf336569e744e47b647c4e
贠乾 <yunqian8@live.com>
2019-10-20 23:10:21 +0800

    新增广告仓储

--- Demo.Core.Api.Repository/AdvertisementRepository.cs
+++ Demo.Core.Api.Repository/AdvertisementRepository.cs
@@ -1,9 +1,13 @@
 ﻿using Demo.Core.Api.IRepository;
+using Demo.Core.Api.Model.Entity;
+using Demo.Core.Api.Model.Seed;
+using SqlSugar;
 using System;
 
 namespace Demo.Core.Api.Repository
 {
-    public class AdvertisementRepository : IAdvertisementRepository
+    public class AdvertisementRepository :BaseRepository<Advertisement>,
+        IAdvertisementRepository
     {
         public int Sum(int i, int j)
         {

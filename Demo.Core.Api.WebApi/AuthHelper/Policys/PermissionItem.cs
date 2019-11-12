a3a022a4b73017147cb1f28c8ce70dcc3214546e
贠乾 <yunqian8@live.com>
2019-11-12 18:35:31 +0800

    1，新增Jwt build 工具类 2，新增基于角色权限处理，角色-链接实体，凭证实体

---
+++ Demo.Core.Api.WebApi/AuthHelper/Policys/PermissionItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Core.Api.WebApi.AuthHelper.Policys
+{
+    /// <summary>
+    /// 用户或角色或其他凭据实体
+    /// </summary>
+    public class PermissionItem
+    {
+        /// <summary>
+        /// 用户或角色或其他凭据名称
+        /// </summary>
+        public virtual string Role { get; set; }
+
+        /// <summary>
+        /// 请求Url
+        /// </summary>
+        public virtual string Url { get; set; }
+    }
+}

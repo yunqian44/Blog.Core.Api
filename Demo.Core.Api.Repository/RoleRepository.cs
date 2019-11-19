98467c1f09fe3ad2dcbfcdc997962815cdfbc7fd
贠乾 <yunqian8@live.com>
2019-11-19 18:46:29 +0800

    新增角色仓储，接口地址仓储

---
+++ Demo.Core.Api.Repository/RoleRepository.cs
@@ -0,0 +1,17 @@
+﻿using Demo.Core.Api.IRepository;
+using Demo.Core.Api.Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Core.Api.Repository
+{
+    /// <summary>
+    /// RoleRepository
+    /// </summary>	
+    public class RoleRepository : BaseRepository<Role>, IRoleRepository
+    {
+
+
+    }
+}

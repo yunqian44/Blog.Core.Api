d25a456a481b028e0bca214b05b10b4e8837902b
贠乾 <yunqian8@live.com>
2019-12-16 18:36:48 +0800

    新增部门控制器 新增用户角色控制器

---
+++ Demo.Core.Api.WebApi/Controllers/DepartmentController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demo.Core.Api.Core.GlobalVar;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Core.Api.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize(Permissions.Name)]
+    public class DepartmentController : ControllerBase
+    {
+        // GET: api/Department
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Department/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Department
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Department/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Demo.Core.Api.WebApi/Controllers/UserRoleController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demo.Core.Api.IServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Core.Api.WebApi.Controllers
+{
+    /// <summary>
+    /// 用户角色关系
+    /// 【无权限】
+    /// </summary>
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class UserRoleController : Controller
+    {
+        readonly ISysUserInfoService _sysUserInfoServices;
+        readonly IUserRoleService _userRoleServices;
+        readonly IRoleService _roleServices;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="sysUserInfoServices"></param>
+        /// <param name="userRoleServices"></param>
+        /// <param name="roleServices"></param>
+        public UserRoleController(ISysUserInfoService sysUserInfoServices, IUserRoleService userRoleServices, IRoleService roleServices)
+        {
+            this._sysUserInfoServices = sysUserInfoServices;
+            this._userRoleServices = userRoleServices;
+            this._roleServices = roleServices;
+        }
+
+
+
+        /// <summary>
+        /// 新建用户
+        /// </summary>
+        /// <param name="loginName"></param>
+        /// <param name="loginPwd"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<object> AddUser(string loginName, string loginPwd)
+        {
+            var model = await _sysUserInfoServices.SaveUserInfo(loginName, loginPwd);
+            return Ok(new
+            {
+                success = true,
+                data = model
+            });
+        }
+
+        /// <summary>
+        /// 新建Role
+        /// </summary>
+        /// <param name="roleName"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<object> AddRole(string roleName)
+        {
+            var model = await _roleServices.SaveRole(roleName);
+            return Ok(new
+            {
+                success = true,
+                data = model
+            });
+        }
+
+        /// <summary>
+        /// 新建用户角色关系
+        /// </summary>
+        /// <param name="uid"></param>
+        /// <param name="rid"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<object> AddUserRole(int uid, int rid)
+        {
+            var model = await _userRoleServices.SaveUserRole(uid, rid);
+            return Ok(new
+            {
+                success = true,
+                data = model
+            });
+        }
+    }
+}

f4cf8a1c5a782eb1a0ad7546e11b2708e78365b0
贠乾 <yunqian8@live.com>
2019-12-16 18:36:08 +0800

    新增全局用户存储信息

---
+++ Demo.Core.Api.Data/HttpContextUser/AspNetUser.cs
@@ -0,0 +1,67 @@
+﻿using Demo.Core.Api.Core.Extension;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace Demo.Core.Api.Data.HttpContextUser
+{
+    public class AspNetUser : IUser
+    {
+        private readonly IHttpContextAccessor _accessor;
+
+        public AspNetUser(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        public string Name => _accessor.HttpContext.User.Identity.Name;
+        public int ID => GetClaimValueByType("jti").FirstOrDefault().ToInt();
+
+        public bool IsAuthenticated()
+        {
+            return _accessor.HttpContext.User.Identity.IsAuthenticated;
+        }
+
+
+        public string GetToken()
+        {
+            return _accessor.HttpContext.Request.Headers["Authorization"].ObjToString().Replace("Bearer ", "");
+        }
+
+        public List<string> GetUserInfoFromToken(string ClaimType)
+        {
+
+            var jwtHandler = new JwtSecurityTokenHandler();
+            if (!string.IsNullOrEmpty(GetToken()))
+            {
+                JwtSecurityToken jwtToken = jwtHandler.ReadJwtToken(GetToken());
+
+                return (from item in jwtToken.Claims
+                        where item.Type == ClaimType
+                        select item.Value).ToList();
+            }
+            else
+            {
+                return new List<string>() { };
+            }
+        }
+
+        public IEnumerable<Claim> GetClaimsIdentity()
+        {
+            return _accessor.HttpContext.User.Claims;
+        }
+
+        public List<string> GetClaimValueByType(string ClaimType)
+        {
+
+            return (from item in GetClaimsIdentity()
+                    where item.Type == ClaimType
+                    select item.Value).ToList();
+
+        }
+    }
+}
---
+++ Demo.Core.Api.Data/HttpContextUser/IUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace Demo.Core.Api.Data.HttpContextUser
+{
+    public interface IUser
+    {
+        string Name { get; }
+        int ID { get; }
+        bool IsAuthenticated();
+        IEnumerable<Claim> GetClaimsIdentity();
+        List<string> GetClaimValueByType(string ClaimType);
+
+        string GetToken();
+        List<string> GetUserInfoFromToken(string ClaimType);
+    }
+}

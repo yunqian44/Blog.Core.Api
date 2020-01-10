bfd67f9ef52c91c81aae0797d684892b1cd0d648
贠乾 <yunqian8@live.com>
2020-01-10 18:19:45 +0800

    新增字符串扩展类

---
+++ Demo.Core.Api.Core/Extension/StringExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Core.Api.Core.Extension
+{
+    public static class StringExtension
+    {
+        public static bool ObjToBool(this string thisValue)
+        {
+            bool reval = false;
+            if (!string.IsNullOrWhiteSpace(thisValue) && bool.TryParse(thisValue, out reval))
+            {
+                return reval;
+            }
+            return reval;
+        }
+    }
+}
